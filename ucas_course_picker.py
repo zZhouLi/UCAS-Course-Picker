28021de83034c6019265ed06477e8ecea5dc0af4
idejie <i@idejie.com>
2019-01-09 15:45:15 +0800

    Update ucas_course_picker.py

--- ucas_course_picker.py
+++ ucas_course_picker.py
@@ -6,7 +6,7 @@ import sys
 
 class Config:
     username = "your_email@email.com"
-    password = "your_passeord"
+    password = "your_password"
     courses = { 
         # "course_code":"dept_id"  
         # like:     "156560": "964"
